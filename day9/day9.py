38214223df7ad234ab3fbe26916727cbd47e108a
Angelo Dipierro <angelo.dipierro@contactlab.com>
2019-12-09 16:18:24 +0100

    Somewhat less quick, dirty & broken fixes to intocde

--- day9/day9.py
+++ day9/day9.py
@@ -15,7 +15,6 @@ def run(memory, p_in=1):
     p_out = []
     done = False
     while not done:
-        print(f'outer memory size: {len(memory)}')
         word = str(memory[ip])
         op, modes = word[-2:].zfill(2), word[:-2].zfill(3)
         if op == '01':
@@ -23,23 +22,19 @@ def run(memory, p_in=1):
             p1, p2, dest = read_p(memory, modes[-1], memory[ip + 1], base), \
                            read_p(memory, modes[-2], memory[ip + 2], base), \
                            memory[ip + 3]
-            memory[dest] = int(p1) + int(p2)
+            write_p(memory, dest, p1 + p2)
             ip += 4
         elif op == '02':
             assert modes[-3] == '0'
             p1, p2, dest = read_p(memory, modes[-1], memory[ip + 1], base), \
                            read_p(memory, modes[-2], memory[ip + 2], base), \
                            memory[ip + 3]
-            if dest > len(memory):
-                memory.extend([0] * (dest - len(memory) + 1))
-            memory[dest] = int(p1) * int(p2)
+            write_p(memory, dest, p1 * p2)
             ip += 4
         elif op == '03':
             assert modes[-1] == '0'
             dest = memory[ip + 1]
-            if dest > len(memory):
-                memory.extend([0] * (dest - len(memory) + 1))
-            memory[dest] = p_in
+            write_p(memory, dest, p_in)
             ip += 2
         elif op == '04':
             p1 = read_p(memory, modes[-1], memory[ip + 1], base)
@@ -54,20 +49,16 @@ def run(memory, p_in=1):
                      read_p(memory, modes[-2], memory[ip + 2], base)
             ip = ip + 3 if p1 else p2
         elif op == '07':
-            p1, p2, dest = read_p(memory, modes[-1], memory[ip + 1]), base, \
-                           read_p(memory, modes[-2], memory[ip + 2]), base, \
+            p1, p2, dest = read_p(memory, modes[-1], memory[ip + 1], base),  \
+                           read_p(memory, modes[-2], memory[ip + 2], base), \
                            memory[ip + 3]
-            if dest > len(memory):
-                memory.extend([0] * (dest - len(memory) + 1))
-            memory[dest] = 1 if p1 < p2 else 0
+            write_p(memory, dest, 1 if p1 < p2 else 0)
             ip += 4
         elif op == '08':
             p1, p2, dest = read_p(memory, modes[-1], memory[ip + 1], base), \
                            read_p(memory, modes[-2], memory[ip + 2], base), \
                            memory[ip + 3]
-            if dest > len(memory):
-                memory.extend([0] * (dest - len(memory) + 1))
-            memory[dest] = 1 if p1 == p2 else 0
+            write_p(memory, dest, 1 if p1 == p2 else 0)
             ip += 4
         elif op == '09':
             p1 = read_p(memory, modes[-1], memory[ip + 1], base)
@@ -80,21 +71,31 @@ def run(memory, p_in=1):
 
 def read_p(memory, mode, address, base=0):
     if mode == '0':
-        if address > len(memory):
-            memory.extend([0] * (address - len(memory) + 1))
+        grow_memory(memory, address)
         return memory[address]
     elif mode == '1':
         return address
     elif mode == '2':
-        if (address + base)  > len(memory):
-            memory.extend([0] * ((address + base) - len(memory) + 1))
+        grow_memory(memory, address + base)
         return memory[address + base]
     else:
         raise AssertionError('invalid address mode')
 
 
+def write_p(memory, address, data):
+    grow_memory(memory, address)
+    memory[address] = data
+
+
+def grow_memory(memory, max_address):
+    mem_size = len(memory)
+    if max_address > mem_size:
+        memory.extend([0] * (mem_size + max_address + 1))
+
+
 def solve_part_1():
-    return run([109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99])
+    return run([109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99])
+
 
 def solve_part_2():
     pass
