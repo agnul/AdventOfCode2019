3e910f94025914df70b3eeab114d72939c3f70f5
Angelo Dipierro <angelo.dipierro@contactlab.com>
2019-12-10 17:28:22 +0100

    I have no idea what I'm doing

--- day10/day10.py
+++ day10/day10.py
@@ -3,20 +3,18 @@ import fileinput
 
 
 def collinear(a, b, c):
-    if ((b[1] - a[1]) / (b[0] - a[0])) == ((c[1] - b[1]) / (c[0] - b[0])):
+    if ((b[1] - a[1]) * (c[0] - b[0]))  == ((c[1] - b[1]) * (b[0] - a[0])):
         return True
     return False
 
 
 def in_between(a, b, c):
-    return a[0] < c[0] < b[0] or a[1] < c[1] < b[1]
+    return a <= c <= b or a <= c <= b
 
 
 field = dict()
 for y in enumerate(fileinput.input('input.txt')):
-    print(y)
     for x in enumerate(y[1].rstrip()):
-        print(x[1])
         if x[1] == '#':
             field[(x[0], y[0])] = []
 
@@ -27,12 +25,17 @@ for a in field:
             if a == b or b == c or a == c:
                 continue
             if collinear(a, b, c):
-                if in_between(a, b, c):
-                    field[a].append(c)
-                elif in_between(a, c, b):
-                    field[a].append(b)
+                if a[0] != b[0]:
+                    if in_between(a[0], c[0], b[0]):
+                        break
+                else:
+                    if in_between(a[1], c[1], b[1]):
+                        break
+            field[a].append(c)
+
 
 if __name__ == '__main__':
     print(field)
+    print(max (len(field[f]) for f in field))
 
 
