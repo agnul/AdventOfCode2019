422a8cc11889473e9a046f25131163fe0b67d6b5
Angelo Dipierro <angelo.dipierro@ext.contactlab.com>
2019-12-15 23:41:44 +0100

    day 14, part 2

--- day14/day14.py
+++ day14/day14.py
@@ -39,12 +39,24 @@ def count_materials(available, q, p):
         return sum(count_materials(available, batches * m[0], m[1]) for m in reactions[p].materials)
 
 
+def maximize_fuel(ore_available):
+    left, right = 0, ore_available
+    while (right - left) > 1:
+        middle = left + (right - left) // 2
+        cost = count_materials(defaultdict(lambda: 0), middle, 'FUEL')
+        if cost > ore_available:
+            right = middle
+        else:
+            left = middle
+    return left
+
+
 def solve_part_1():
     return count_materials(defaultdict(lambda: 0), 1, 'FUEL')
 
 
 def solve_part_2():
-    pass
+    return maximize_fuel(1_000_000_000_000)
 
 
 if __name__ == '__main__':
@@ -62,6 +74,16 @@ if __name__ == '__main__':
                    '5 B, 7 C => 1 BC',
                    '4 C, 1 A => 1 CA',
                    '2 AB, 3 BC, 4 CA => 1 FUEL']
+
+    third_test = ['157 ORE => 5 NZVS',
+                  '165 ORE => 6 DCFZ',
+                  '44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL',
+                  '12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ',
+                  '179 ORE => 7 PSHF',
+                  '177 ORE => 5 HKGWZ',
+                  '7 DCFZ, 7 PSHF => 2 XJWVT',
+                  '165 ORE => 2 GPVTF',
+                  '3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT']
 
     reactions = parse_reactions(open('input.txt', 'r'))
 
